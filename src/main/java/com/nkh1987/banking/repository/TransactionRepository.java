e5a6b1ac04a79c6c128406381fe3613d63f83f5d
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-08-24 12:38:15 +0330

    Add Transaction entity

--- src/main/java/com/nkh1987/banking/repository/TransactionRepository.java
+++ src/main/java/com/nkh1987/banking/repository/TransactionRepository.java
@@ -4,4 +4,5 @@ import com.nkh1987.banking.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
+
 }
