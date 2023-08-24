2e23f9eafa8607df14787bfe696f4e0fc3736f9b
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-08-24 10:30:52 +0330

    Add swagger

--- src/main/java/com/nkh1987/banking/BankingApplication.java
+++ src/main/java/com/nkh1987/banking/BankingApplication.java
@@ -1,9 +1,34 @@
 package com.nkh1987.banking;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Banking application",
+				description = "Spring Boot backend along with docker and swagger",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Nima Khodabande",
+						email = "nima.khodabandelou@gmail.com",
+						url = "https://github.com/Nima-Khodabandelou"
+				),
+				license = @License(
+						name = "Banking app",
+						url = "https://github.com/Nima-Khodabandelou/java_spring_banking_application"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot banking app",
+				url = "https://github.com/Nima-Khodabandelou/java_spring_banking_application"
+		)
+)
 public class BankingApplication {
 
 	public static void main(String[] args) {
